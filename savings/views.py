65ea351db487fd45e06720d158bb430ca0d03d7a
Delta.v <tmm.michalski@gmail.com>
2022-08-14 14:37:46 +0200

    format entry dates, titles and values

--- savings/views.py
+++ savings/views.py
@@ -2,15 +2,23 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Entry
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Sum
 
 @login_required(redirect_field_name='')
 def index(request):
     # if not request.user.is_authenticated:
     #     return redirect('login:login')
-    entries = Entry.objects.all()
+    if request.method == 'POST':
+        title = request.POST['title']
+        value = request.POST['value']
+        entry = Entry(title=title, value=value, owner=request.user)
+        entry.save()
+
+    entries = Entry.objects.filter(owner=request.user).order_by('-created_at')
+    value_sum = Entry.objects.filter(owner=request.user).aggregate(Sum('value'))
     context = {
-        'entries': entries
+        'entries': entries,
+        'value_sum': value_sum['value__sum'],
     }
     return render(request, 'savings/index.html', context)
 
