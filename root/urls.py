9799488df28bc2111c67a8132bc048e7a75f0064
Delta.v <tmm.michalski@gmail.com>
2022-08-16 11:24:05 +0200

    Setup basic mail reset system for users

--- root/urls.py
+++ root/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('login.urls')),
     path('main-view/', include('savings.urls')),
     path('admin/', admin.site.urls),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
